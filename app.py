a5a27163bff1837ecc5d4f79926b5f41bf5b3bbd
MD Shakib <shakibahmed.528874@gmail.com>
2023-09-17 13:52:17 +0600

    solved img warning

--- app.py
+++ app.py
@@ -1,56 +1,54 @@
-from tkinter import *
 import customtkinter as ctk
-from PIL import Image, ImageTk
+from PIL import Image
 from notepad import Note
 
 
 class NoteBook:
     def __init__(self):
         '''initially create button -> add note & add label'''
-        img = Image.open("Images/plus.png")
-        img = img.resize((30, 30))
-        img = ImageTk.PhotoImage(image=img)
+        img = ctk.CTkImage(light_image=Image.open(
+            "Images/add.png"), size=(30, 30))
         btn = ctk.CTkButton(navFrame, image=img, text="New Note", fg_color="#222", hover_color="#333", font=(
-            "Arial", 18, "bold"), width=200,height=50, text_color="#aaa", corner_radius=50, command=self.newNote)
+            "Arial", 18, "bold"), width=200, height=50, text_color="#aaa", corner_radius=50, command=self.newNote)
         btn.pack(pady=5)
 
         # label
         ctk.CTkLabel(navFrame, text="Labels", text_color="#aaa").pack()
-        img_lbl = Image.open("Images/plus2.png")
-        img_lbl = img_lbl.resize((20, 20))
-        img_lbl = ImageTk.PhotoImage(image=img_lbl)
+        img_lbl = ctk.CTkImage(light_image=Image.open(
+            "Images/add2.png"), size=(20, 20))
         btn = ctk.CTkButton(navFrame, image=img_lbl, text="Add Label", fg_color="#222", hover_color="#333", font=(
             "Arial", 14, "bold"), text_color="#aaa", height=50, width=200, corner_radius=50, command=self.labelTitle)
         btn.pack()
         
+    def button(self, path: str,name: str, cmd):
+        img = ctk.CTkImage(light_image=Image.open(path), size=(30,30))
+        btn = ctk.CTkButton(navFrame, image=img, text=name, fg_color="#222", hover_color="#333", font=(
+            "Arial", 18, "bold"), width=200,height=50, text_color="#aaa", corner_radius=50, command=cmd)
+        btn.pack(pady=5)
+        
+
     def newNote(self):
         note = Note()
         note.mainloop()
-        
-        
-        
-        
 
     def labelTitle(self):
         '''this method call the addLabel method and give title of label'''
         dialog = ctk.CTkInputDialog(text="Label Name...", title='New Label', )
         title = dialog.get_input()
-        
+
         if title == '':
             title = "New-1"
 
         self.addLabel(title)
 
     def addLabel(self, title):
-        img = Image.open("Images/label.png")
-        img = img.resize((40, 30))
-        img = ImageTk.PhotoImage(image=img)
+        img = ctk.CTkImage(light_image=Image.open(
+            "Images/label.png"), size=(40, 30))
         btn = ctk.CTkButton(navFrame, image=img, fg_color="#222", hover_color="#444", corner_radius=50, text=title,
                             command=self.labelTitle, height=25, width=200, font=("Arial", 16, "italic"), text_color="#aaa")
         btn.pack()
 
-
-
+    
 
 
 root = ctk.CTk()
@@ -65,6 +63,6 @@ navFrame = ctk.CTkScrollableFrame(
 navFrame.pack(side="left", fill="y")
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     NoteBook()
     root.mainloop()
