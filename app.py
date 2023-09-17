6b3ee75d8d526fe46f5f6bbadb3f0fbf12aca463
MD Shakib <shakibahmed.528874@gmail.com>
2023-09-17 22:21:17 +0600

    added .data dir

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import customtkinter as ctk
 from tkinter import messagebox
 from PIL import Image
+import os
 from notepad import Note
 
 
 class NoteBook:
     def __init__(self):
         '''initially create button -> add note & add label'''
+        if not os.path.exists(".data"):
+            os.mkdir(".data")
+        
         self.button(navFrame, "Images/add.png", "New Note",
-                    (30, 30), self.newNote)  # Add note button
+                    (30, 30), self.newNote)  # Add note
+        self.button(navFrame, "Images/archive.png", "Archive\t",
+                    (30, 30), self.archive)  # archive
+        self.button(navFrame, "Images/delete.png",
+                    "Trash\t", (30, 30), self.trash)  # trash
+        self.button(navFrame, "Images/setting.png", "Settings\t",
+                    (30, 30), self.settings)  # settings
         ctk.CTkLabel(navFrame, text="Labels",
-                     text_color="#aaa").pack()  # label heading
+                     text_color="#aaa", height=40).pack()  # label heading
         self.button(navFrame, "Images/add2.png", "Add Label",
                     (20, 20), self.labelTitle)  # label Button
 
@@ -19,7 +29,7 @@ class NoteBook:
         img = ctk.CTkImage(light_image=Image.open(path), size=imgSize)
         btn = ctk.CTkButton(master=window, image=img, text=name, fg_color="#222", hover_color="#333", font=(
             "Arial", 18, "bold"), width=200, height=50, text_color="#aaa", corner_radius=50, command=cmd)
-        btn.pack(pady=5)
+        btn.pack(pady=2)
 
     def labelTitle(self):
         '''this method call the addLabel method and give title of label'''
@@ -32,6 +42,19 @@ class NoteBook:
         self.addLabel(title)
 
     def addLabel(self, title):
+        '''this function add the new label(directory) and make a file as name of directory'''
+        
+        if not os.path.exists(f".data/.{title}"):
+            os.mkdir(f".data/.{title}")
+            
+            with open(f".data/.{title}.txt", "w") as f:
+                f.write("Success")
+            
+        else:
+            messagebox.showerror("error", "Label Alrady Exists.")
+        
+        
+        
         img = ctk.CTkImage(light_image=Image.open(
             "Images/label.png"), size=(40, 30))
         btn = ctk.CTkButton(navFrame, image=img, fg_color="#222", hover_color="#444", corner_radius=50, text=title,
@@ -44,9 +67,17 @@ class NoteBook:
 
         if title == '':
             title = "New Note - 1"
-        
 
         new_note = Note(title)
+
+    def settings(self):
+        pass
+
+    def trash(self):
+        pass
+
+    def archive(self):
+        pass
 
 
 class Note(ctk.CTkToplevel):
@@ -66,21 +97,21 @@ class Note(ctk.CTkToplevel):
 
         # make save and delete button
         undoBtn = self.button(
-            self.noteFrame, "Images/undo.png", "Undo", (40, 40), self.undo)
+            self.noteFrame, "Images/undo.png", "Undo\t", (40, 40), self.undo)
         redoBtn = self.button(
-            self.noteFrame, "Images/redo.png", "Redo", (40, 40), self.redo)
+            self.noteFrame, "Images/redo.png", "Redo\t", (40, 40), self.redo)
         saveBtn = self.button(
-            self.noteFrame, "Images/add2.png", "Save", (30, 30), self.save)
+            self.noteFrame, "Images/add2.png", "Save\t", (30, 30), self.save)
         clrBtn = self.button(
-            self.noteFrame, "Images/eraser.png", "Clear", (30, 30), self.clear)
+            self.noteFrame, "Images/eraser.png", "Clear\t", (30, 30), self.clear)
         zoomIn = self.button(
-            self.noteFrame, "Images/zoom-in.png", "Zoom In", (20, 20), self.zoomIn)
+            self.noteFrame, "Images/zoom-in.png", "Zoom In\t", (20, 20), self.zoomIn)
         zoomIn = self.button(
             self.noteFrame, "Images/zoom-out.png", "Zoom Out", (20, 20), self.zoomOut)
         helpBtn = self.button(
-            self.noteFrame, "Images/help.png", "Help", (30, 30), self.help)
+            self.noteFrame, "Images/help.png", "Help\t", (30, 30), self.help)
         closeBtn = self.button(
-            self.noteFrame, "Images/close.png", "Close", (30, 30), self.destroy)
+            self.noteFrame, "Images/close.png", "Close\t", (30, 30), self.destroy)
 
     def button(self, window, path: str, name: str, imgSize: tuple, cmd):
         '''this is default button template'''
